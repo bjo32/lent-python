01e174d58ad762ab513d6aa2ff4dcb6821149638
bjo32 <bjo32@cam.ac.uk>
2026-02-11 11:49:57 +0000

    annotations added

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -14,6 +14,9 @@ from floodsystem.stationdata import build_station_list
 
 build_station_list(use_cache=True)
 from haversine import haversine, Unit
+"""this function takes an imput of the list of stations and a tuple for coordinates p,
+then makes a list of tuples containing the station and the distance to p in km. 
+finnaly the list is sorted by distance in ascending order."""
 def stations_by_distance(stations, p):
     sorted_tuples = []
     for station in stations:
@@ -22,6 +25,10 @@ def stations_by_distance(stations, p):
     sorted_tuples = sorted_by_key(sorted_tuples, 1)
     return sorted_tuples
 
+"""this function takes the list of stations, a tuple of coordinates for centre, float for radius r
+values is an intermediate list of tuples created using the function 'stations_by_distance' of ordered stations by distance from centre
+the items in the list with a redius less than r are added to a list called 'output', since values was ordered
+the output is also ordered."""
 def stations_within_radius(stations, centre, r):
     values = stations_by_distance(stations, centre)
     output = []
