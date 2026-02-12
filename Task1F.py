fbe19b6aba09b43e9d8ffe54ddfe05a215c01eba
HannahWangyihan <yw835@cam.ac.uk>
2026-02-12 12:44:50 +0000

    1F

--- Task1F.py
+++ Task1F.py
@@ -2,6 +2,25 @@
 from floodsystem.station import inconsistent_typical_range_stations
 from floodsystem.stationdata import build_station_list
 
+def test_1f():
+
+    
+    s_good = DummyStation("Good", "River X", (1.0, 2.0))
+    
+    s_bad_logic = DummyStation("Bad Logic", "River X", (2.0, 1.0))  # Inconsistent station
+    
+    # Inconsistent station with None data
+    s_bad_none = DummyStation("Bad None", "River X", None)
+    
+    # Partial None
+    s_bad_partial = DummyStation("Bad Partial", "River X", (1.0, None))
+
+    stations = [s_good, s_bad_logic, s_bad_none, s_bad_partial]
+    assert s_good.typical_range_consistent() == True
+    assert s_bad_logic.typical_range_consistent() == False
+    assert s_bad_none.typical_range_consistent() == False
+
+
 def run():
 
     stations = build_station_list()
