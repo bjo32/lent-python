91a8fae8cdd7c4714450dc39b60406f46c8a1f6d
bjo32 <bjo32@cam.ac.uk>
2026-02-12 12:39:45 +0000

    changes to test_geo

--- test_geo.py
+++ test_geo.py
@@ -2,15 +2,15 @@ from floodsystem.geo import haversine, stations_by_distance, rivers_with_station
 from floodsystem.stationdata import build_station_list
 def test_stations_by_distance():
      stations = build_station_list()
-p = (52.2053, 0.1218)
-distance_list = stations_by_distance(stations, p)
-assert len(distance_list) == len(stations)
-assert distance_list[0][0].name == "Cambridge Jesus Lock"
+     p = (52.2053, 0.1218)
+     distance_list = stations_by_distance(stations, p)
+     assert len(distance_list) == len(stations)
+     assert distance_list[0][0].name == "Cambridge Jesus Lock"
 
 def test_stations_within_radius(): 
      stations = build_station_list()
      p = (52.2053, 0.1218)
      radius = 10
      stations_within_radius = [s for s in stations if haversine(p, s.coord) < radius]
-assert len(stations_within_radius) > 0
-assert any(s.name == "Cambridge Jesus Lock" for s in stations_within_radius)
+     assert len(stations_within_radius) > 0
+     assert any(s.name == "Cambridge Jesus Lock" for s in stations_within_radius)
